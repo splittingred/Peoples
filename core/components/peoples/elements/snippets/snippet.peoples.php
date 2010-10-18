c36bda795ce9237eb9ead8a99646d889e0b62af0
Shaun McCormick <shaun@modxcms.com>
2010-10-18 16:44:45 -0500

    Finished Peoples snippet

--- core/components/peoples/elements/snippets/snippet.peoples.php
+++ core/components/peoples/elements/snippets/snippet.peoples.php
@@ -1,6 +1,28 @@
 <?php
 /**
+ * Peoples
+ *
+ * Copyright 2010 by Shaun McCormick <shaun@modx.com>
+ *
+ * Peoples is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Peoples is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Peoples; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package peoples
+ */
+/**
  * Displays a list of Users
+ *
+ * @package peoples
  */
 $peoples = $modx->getService('peoples','Peoples',$modx->getOption('peoples.core_path',null,$modx->getOption('core_path').'components/peoples/').'model/peoples/',$scriptProperties);
 if (!($peoples instanceof Peoples)) return '';
@@ -13,37 +35,41 @@ $usergroups = $modx->getOption('usergroups',$scriptProperties,'');
 $limit = $modx->getOption('limit',$scriptProperties,10);
 $start = $modx->getOption('start',$scriptProperties,0);
 $sortBy = $modx->getOption('sortBy',$scriptProperties,'username');
+$sortByAlias = $modx->getOption('sortByAlias',$scriptProperties,'User');
 $sortDir = $modx->getOption('sortDir',$scriptProperties,'ASC');
 $cls = $modx->getOption('cls',$scriptProperties,'ppl-user');
-$altCls = $modx->getOption('altCls',$scriptProperties,'ppl-user-alt');
+$altCls = $modx->getOption('altCls',$scriptProperties,'');
 $firstCls = $modx->getOption('firstCls',$scriptProperties,'');
 $lastCls = $modx->getOption('lastCls',$scriptProperties,'');
 $placeholderPrefix = $modx->getOption('placeholderPrefix',$scriptProperties,'peoples.');
 $profileAlias = $modx->getOption('profileAlias',$scriptProperties,'Profile');
+$userClass = $modx->getOption('userClass',$scriptProperties,'modUser');
+$userAlias = $modx->getOption('userAlias',$scriptProperties,'User');
 
 /* build query */
-$c = $modx->newQuery('modUser');
+$c = $modx->newQuery($userClass);
+$c->setClassAlias($userAlias);
 if (is_bool($active) || $active < 2) {
     $c->where(array(
-        'modUser.active' => $active,
+        $userAlias.'.active' => $active,
     ));
 }
 /* filter by user groups */
 if (!empty($usergroups)) {
     $usergroups = explode(',',$usergroups);
-    $c->leftJoin('modUserGroupMember','UserGroupMembers');
+    $c->leftJoin('modUserGroupMember','UserGroupMembers',$modx->getSelectColumns($userClass,$userAlias,'',array('id')).' = '.$modx->getSelectColumns('modUserGroupMember','UserGroupMembers','',array('member')));
     $c->leftJoin('modUserGroup','UserGroup',$modx->getSelectColumns('modUserGroupMember','UserGroupMembers','',array('user_group')).' = '.$modx->getSelectColumns('modUserGroup','UserGroup','',array('id')));
     $c->where(array(
         'UserGroup.name:IN' => $usergroups,
     ));
 }
-$count = $modx->getCount('modUser',$c);
-$c->sortby($modx->escape($sortBy),$sortDir);
+$count = $modx->getCount($userClass,$c);
+$c->sortby($sortByAlias.'.'.$sortBy,$sortDir);
 if (!empty($limit)) {
     $c->limit($limit,$start);
 }
 $c->bindGraph('{"'.$profileAlias.'":{}}');
-$users = $modx->getCollectionGraph('modUser','{"'.$profileAlias.'":{}}',$c);
+$users = $modx->getCollectionGraph($userClass,'{"'.$profileAlias.'":{}}',$c);
 
 /* iterate */
 $list = array();
@@ -56,7 +82,7 @@ foreach ($users as $user) {
     $userArray = $user->get(array('id','username','active','class_key','remote_key','remote_data'));
     $userArray = array_merge($user->Profile->toArray(),$userArray);
     
-    $userArray['cls'] = $alt ? $altCls : $cls;
+    $userArray['cls'] = $alt && !empty($altCls) ? $cls.' '.$altCls : $cls;
     if (!empty($firstCls) && $idx == 0) {
         $userArray['cls'] .= ' '.$firstCls;
     }
--- core/components/peoples/lexicon/en/properties.inc.php
+++ core/components/peoples/lexicon/en/properties.inc.php
@@ -1,11 +1,51 @@
 <?php
 /**
+ * Peoples
+ *
+ * Copyright 2010 by Shaun McCormick <shaun@modx.com>
+ *
+ * Peoples is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Peoples is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Peoples; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package peoples
+ */
+/**
  * English language properties topic
  * 
  * @package peoples
  * @subpackage lexicon
  * @language en
  */
+
+/* options */
+$_lang['peoples.opt_asc'] = 'Ascending';
+$_lang['peoples.opt_desc'] = 'Descending';
+
+/* Peoples */
 $_lang['prop_peoples.tpl_desc'] = 'The Chunk to use for each User.';
 $_lang['prop_peoples.active_desc'] = '0 to show only inactive Users, 1 to show only active Users, 2 to show all Users.';
 $_lang['prop_peoples.usergroups_desc'] = 'Optional. A comma-separated list of User Group names to filter by.';
+$_lang['prop_peoples.limit'] = 'The number of users to limit per call. Defaults to 10. Set to 0 to show all.';
+$_lang['prop_peoples.start'] = 'The start index to begin with when limiting.';
+$_lang['prop_peoples.sortby'] = 'The field name to sort by. (Cannot be an extended field.)';
+$_lang['prop_peoples.sortbyalias'] = 'The class to use with the sort field.';
+$_lang['prop_peoples.sortdir'] = 'The direction to sort by.';
+$_lang['prop_peoples.cls'] = 'Optional. If set, will append this CSS class to each item.';
+$_lang['prop_peoples.altcls'] = 'Optional. If set, will append this CSS class to every even item.';
+$_lang['prop_peoples.firstcls'] = 'Optional. If set, will append this CSS class to the first item.';
+$_lang['prop_peoples.lastcls'] = 'Optional. If set, will append this CSS class to the last item.';
+$_lang['prop_peoples.placeholderprefix_desc'] = 'The prefix to use when setting global placeholders, such as total.';
+$_lang['prop_peoples.outputseparator_desc'] = 'The separator between each user record.';
+$_lang['prop_peoples.toplaceholder_desc'] = 'Optional. If set, will set the output to this placeholder and return empty.';
+$_lang['prop_peoples.userclass_desc'] = 'The class name of the Users object.';
+$_lang['prop_peoples.useralias_desc'] = 'The class alias of the Users object.';
